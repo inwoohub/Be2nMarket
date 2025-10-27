57f3ba979f3b8b9be079e2dc7484237aacddd246
HwangInWoo <dlsdndls@naver.com>
2025-10-28 01:44:47 +0900

    frontend test

---
+++ frontend/src/pages/Index.js
@@ -0,0 +1,27 @@
+
+import "../css/Index.css";
+
+function Index() {
+
+    return (
+        <div className="Indexpage">
+            <div className="subIndexpage">
+                <div className="textIndexpagediv">
+                    <span className="spanIndexpage">
+                        무한루프 장터에 오신 것을 환영합니다.
+                    </span>
+                </div>
+                <div className="textIndexpagediv2">
+                    <span className="spanIndexpage">
+                        대한민국 No.1 중고 플랫폼
+                    </span>
+                </div>
+                <div className="Login_input">
+                    <img className="kakao_login" alt="카카오 로그인" src="/kakao_login.png" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Index;
